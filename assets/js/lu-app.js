d74dfc93f9f558dbdeb6f60ce6087214cb64283c
luisurruela <luis.urruela@gmail.com>
2020-04-04 11:03:22 -0600

    scripts fixed

--- assets/js/lu-app.js
+++ assets/js/lu-app.js
@@ -1,8 +1,14 @@
-new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 1,
-    autoplay: 6000,
-    hoverPause: true,
-    animationDuration: 1000,
-}).mount()
+window.addEventListener('DOMContentLoaded', inicio);
+
+function inicio() {
+
+    new Glide('.glide', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 1,
+        autoplay: 6000,
+        hoverPause: true,
+        animationDuration: 1000,
+    }).mount()
+    
+}
