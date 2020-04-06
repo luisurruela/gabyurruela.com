b3d6f3b9cc6746ebca5c00f7c19e379540f068b8
luisurruela <luis.urruela@gmail.com>
2020-04-06 10:03:26 -0500

    fade slider added

--- assets/js/lu-app.js
+++ assets/js/lu-app.js
@@ -2,13 +2,9 @@ window.addEventListener('DOMContentLoaded', inicio);
 
 function inicio() {
 
-    new Glide('.glide', {
-        type: 'carousel',
-        startAt: 0,
-        perView: 1,
-        autoplay: 6000,
-        hoverPause: true,
-        animationDuration: 1000,
-    }).mount()
+    $('.carousel').carousel({
+        interval: 2500,
+        pause: 'hover'
+    })
     
 }
